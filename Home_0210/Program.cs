7b4d7186cee7d8ac2177162f2034935382e5eb89
shipilo <shadowfire-1@mail.ru>
2021-10-02 10:12:52 +0300

    Добавьте файлы проекта.

---
+++ Home_0210/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Home_0210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Met_0210/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Met_0210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
